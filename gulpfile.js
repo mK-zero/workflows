de7f938c71bfe2ea23a7379daa22ab14b0e990bb
Marvin <mmelitan@asu.edu>
2016-07-17 04:35:37 -0700

    Minified our HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	gulpif = require('gulp-if');
 	uglify = require('gulp-uglify');
+	minifyHTML = require('gulp-minify-html');
 	concat = require('gulp-concat');
 
 var env,
@@ -70,7 +71,7 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch(jsonSources, ['json']);
 });
 
@@ -82,13 +83,15 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-	gulp.src(htmlSources)
-		.pipe(connect.reload());
+	gulp.src('builds/development/*.html')
+		.pipe(gulpif(env === 'production', minifyHTML()))
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+		.pipe(connect.reload())
 });
 
 gulp.task('json', function() {
 	gulp.src(jsonSources)
-		.pipe(connect.reload());
+		.pipe(connect.reload())
 });
 
 gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
